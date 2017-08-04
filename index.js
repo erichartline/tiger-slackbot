1e51f97293ede2a9f32d2ee592e06b4612bbd202
wildlifehexagon <crossthebreeze@gmail.com>
2017-08-04 13:44:58 -0400

    api testing

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const url = "question-data.json";
+
 app.post('/', (req, res) => {
-  let text = req.body.text;
+  // let text = req.body.text;
   // conditional to check if command is correct
   // if(! /^\d+$/.test(q.text)) { // not a digit
   //   res.send('U R DOIN IT WRONG. Enter a status code like 200!');
@@ -15,15 +17,32 @@ app.post('/', (req, res) => {
 
   // if data is legit, send response
   // would make sense to set this as its own file once it grows larger
-  let data = {
-    response_type: 'in_channel', // public to the channel
-    text: '302: Found',
-    attachments:[
-      {
-        image_url: 'https://http.cat/302.jpg'
-      }
-  ]};
-  res.json(data);
+  // let data = {
+  //   response_type: 'in_channel', // public to the channel
+  //   text: '302: Found',
+  //   attachments:[
+  //     {
+  //       image_url: 'https://http.cat/302.jpg'
+  //     }
+  // ]};
+  fetch(url)
+  .then(function(data) {
+    let question = data.question;
+    let category = data.category;
+    let source = data.source;
+
+    let response = {
+      question: question,
+      category: category,
+      source: source
+    }
+
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+
+  res.json(response);
 });
 
 const server = app.listen(8000, () => {
