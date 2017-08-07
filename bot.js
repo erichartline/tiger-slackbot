66145346d2fda4e2ba9b8aca174fcfd4d1a3ac98
wildlifehexagon <crossthebreeze@gmail.com>
2017-08-07 15:09:17 -0400

    add config file

--- bot.js
+++ bot.js
@@ -6,8 +6,10 @@ const MemoryDataStore = require('@slack/client').MemoryDataStore;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 // Import the client event constants from the Slack API
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-
-const token = '';
+// require config with API keys
+let config = require('./config');
+// set API token from config.js
+let token = config.api.token;
 
 // The Slack constructor takes 2 arguments:
 // token - String representation of the Slack token
