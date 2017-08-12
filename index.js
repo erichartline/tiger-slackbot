903802e45c45b7b49e945aa7b1f7815555bca680
wildlifehexagon <crossthebreeze@gmail.com>
2017-08-12 12:36:10 -0500

    remove commented code

--- index.js
+++ index.js
@@ -27,46 +27,5 @@ bot.respondTo('help', (message, channel) => {
 
 const url = "question-data.json";
 
-
-
-// app.post('/', (req, res) => {
-  // let text = req.body.text;
-  // conditional to check if command is correct
-  // if(! /^\d+$/.test(q.text)) { // not a digit
-  //   res.send('U R DOIN IT WRONG. Enter a status code like 200!');
-  //   return;
-  // }
-
-  // if data is legit, send response
-  // would make sense to set this as its own file once it grows larger
-  // let data = {
-  //   response_type: 'in_channel', // public to the channel
-  //   text: '302: Found',
-  //   attachments:[
-  //     {
-  //       image_url: 'https://http.cat/302.jpg'
-  //     }
-  // ]};
-
-//   res.json(response);
-// });
-//
-// fetch(url)
-// .then(function(data) {
-//   let question = data.question;
-//   let category = data.category;
-//   let source = data.source;
-//
-//   let response = {
-//     question: question,
-//     category: category,
-//     source: source
-//   }
-//
-// })
-// .catch(function(err) {
-//   console.log(err);
-// })
-
 const server = app.listen(8000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
