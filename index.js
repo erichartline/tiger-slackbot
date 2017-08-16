bc094b482b0fdf01c42613c7fc508d8574be587a
wildlifehexagon <crossthebreeze@gmail.com>
2017-08-16 14:58:31 -0500

    revert token

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // require config with API keys
 let config = require('./config');
 // set API token from config.js
-let token = process.env['SLACK_TEAM_TOKEN'];
+let token = config.api.token;
 
 //JSON data async load and format
 const fs = require('fs');
