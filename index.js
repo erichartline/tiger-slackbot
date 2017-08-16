bebc2b8969339ee107568ce1f36640d2eb741099
wildlifehexagon <crossthebreeze@gmail.com>
2017-08-16 14:33:52 -0500

    heroku bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const Bot = require('./Bot');
+const Bot = require('bot.js');
 const app = express();
 
 app.use(bodyParser.json());
